403bc484e31f2df638d7eb86c36202f0b2556e8e
Tracy Norris <tknorris@gmail.com>
2015-01-25 22:43:51 -0500

    increase source cache to reduce load on site

--- scrapers/movietv_scraper.py
+++ scrapers/movietv_scraper.py
@@ -56,7 +56,7 @@ class MovieTV_Scraper(scraper.Scraper):
         hosters = []
         if source_url:
             url = urlparse.urljoin(self.base_url, source_url)
-            html = self._http_get(url, cache_limit=.5)
+            html = self._http_get(url, cache_limit=1)
             if video.video_type == VIDEO_TYPES.MOVIE:
                 pattern = 'show.start\((.*?)\);'
                 quality = QUALITIES.HD
