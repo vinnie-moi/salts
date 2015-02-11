9046ead3b4e8c82aa9973854bec65a9af0219022
Tracy Norris <tknorris@gmail.com>
2015-02-10 23:31:33 -0500

    migrate vidics to new algorithm

--- scrapers/vidics_scraper.py
+++ scrapers/vidics_scraper.py
@@ -100,7 +100,7 @@ class Vidics_Scraper(scraper.Scraper):
     def _get_episode_url(self, show_url, video):
         episode_pattern = 'href="(/Serie/[^-]+-Season-%s-Episode-%s)' % (video.season, video.episode)
         title_pattern = 'class="episode"\s+href="([^"]+).*?class="episode_title">\s*-\s*(.*?) \('
-        airdate_pattern = 'class="episode"\s+href="([^"]+)(?:[^>]+>){2}[^<]+\s+\({year} {month_name} {day}\)'
+        airdate_pattern = 'class="episode"\s+(?:style="[^"]+")?\s+href="([^"]+)(?:[^>]+>){2}[^<]+\s+\({year} {month_name} {p_day}\)'
         return super(Vidics_Scraper, self)._default_get_episode_url(show_url, video, episode_pattern, title_pattern, airdate_pattern)
 
     def _http_get(self, url, data=None, cache_limit=8):
