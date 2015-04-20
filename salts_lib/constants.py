8fdc08594a830460f149aff9aade0b2f46cee9ab
Tracy Norris <tknorris@gmail.com>
2015-04-20 02:03:31 -0400

    update host qualities

--- salts_lib/constants.py
+++ salts_lib/constants.py
@@ -71,7 +71,7 @@ BLOG_Q_MAP[QUALITIES.HD] = ['720', '1080']
 HOST_Q = {}
 HOST_Q[QUALITIES.LOW] = ['youwatch', 'allmyvideos', 'played.to', 'gorillavid']
 HOST_Q[QUALITIES.MEDIUM] = ['primeshare', 'exashare', 'bestreams', 'flashx', 'vidto', 'vodlocker', 'thevideo', 'vidzi', 'vidbull', 'realvid', 'nosvideo', 'daclips', 'sharerepo', 'zalaa']
-HOST_Q[QUALITIES.HIGH] = ['vidspot', 'mrfile', 'divxstage']
+HOST_Q[QUALITIES.HIGH] = ['vidspot', 'mrfile', 'divxstage', 'streamcloud']
 HOST_Q[QUALITIES.HD] = ['thefile', 'sharesix', 'mightyupload', 'filenuke', 'hugefiles', '180upload', 'vidxden', 'billionuploads', 'movshare', 'nowvideo']
 
 Q_ORDER = {QUALITIES.LOW: 1, QUALITIES.MEDIUM: 2, QUALITIES.HIGH: 3, QUALITIES.HD: 4}
