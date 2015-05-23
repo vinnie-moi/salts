b4c6370a3cb22698476cbf88e8a8c955721ca7d4
Tracy Norris <tknorris@gmail.com>
2015-05-23 19:07:49 -0400

    add progress tracking to no multimode

--- default.py
+++ default.py
@@ -1159,28 +1159,29 @@ def set_related_url(mode, video_type, title, year, slug, season='', episode='', 
     if max_timeout == 0: timeout = None
     worker_count = 0
     workers = []
+    fails = {}
     if utils.P_MODE != P_MODES.NONE: q = utils.Queue()
     begin = time.time()
     video = ScraperVideo(video_type, title, year, slug, season, episode, ep_title, ep_airdate)
     with gui_utils.ProgressDialog('Set Related Url', utils.make_progress_msg(video_type, title, year, season, episode)) as pd:
-        for cls in utils.relevant_scrapers(video_type, order_matters=True):
+        scrapers = utils.relevant_scrapers(video_type, order_matters=True)
+        total = len(scrapers)
+        result_count = 0
+        for cls in scrapers:
             scraper = cls(max_timeout)
             if utils.P_MODE == P_MODES.NONE:
-                related = {}
-                related['class'] = scraper
-                url = related['class'].get_url(video)
+                url = scraper.get_url(video)
                 if not url: url = ''
-                related['url'] = url
-                related['name'] = related['class'].get_name()
-                related['label'] = '[%s] %s' % (related['name'], related['url'])
-                related_list.append(related)
+                result_count += 1
+                pd.update(result_count * 100 / total, line2='Received result from: %s' % (scraper.get_name()))
             else:
+                url = ''
                 worker = utils.start_worker(q, utils.parallel_get_url, [scraper, video])
                 utils.increment_setting('%s_try' % (cls.get_name()))
                 worker_count += 1
                 workers.append(worker)
-                related = {'class': scraper, 'name': cls.get_name(), 'label': '[%s]' % (cls.get_name()), 'url': ''}
-                related_list.append(related)
+            related = {'class': scraper, 'url': url, 'name': cls.get_name(), 'label': '[%s] %s' % (cls.get_name(), url)}
+            related_list.append(related)
     
         # collect results from workers
         if utils.P_MODE != P_MODES.NONE:
