c799a3d0e37c22a52ba669ab0f62d81a5eee0201
Tracy Norris <tknorris@gmail.com>
2015-05-25 17:56:28 -0400

    add dk's themes

--- salts_lib/utils.py
+++ salts_lib/utils.py
@@ -69,7 +69,8 @@ list_size = int(ADDON.get_setting('list_size'))
 trakt_api = Trakt_API(TOKEN, use_https, list_size, trakt_timeout)
 db_connection = DB_Connection()
 
-THEME_LIST = ['Shine', 'Luna_Blue', 'Iconic', 'Simple', 'SALTy']
+THEME_LIST = ['Shine', 'Luna_Blue', 'Iconic', 'Simple', 'SALTy', 'SALTy (Blended)', 'SALTy (Blue)', 'SALTy (Frog)', 'SALTy (Green)',
+              'SALTy (Macaw)', 'SALTier (Green)', 'SALTier (Orange)', 'SALTier (Red)']
 THEME = THEME_LIST[int(ADDON.get_setting('theme'))]
 if xbmc.getCondVisibility('System.HasAddon(script.salts.themepak)'):
     themepak_path = xbmcaddon.Addon('script.salts.themepak').getAddonInfo('path')
