ccdfd35bb2a30885ac578e985303c6d4f17f7660
Tracy Norris <tknorris@gmail.com>
2015-06-12 15:48:32 -0400

    add shush to auto-conf

--- default.py
+++ default.py
@@ -157,8 +157,8 @@ def auto_conf():
         _SALTS.set_setting('sort3_field', '1')
         _SALTS.set_setting('sort4_field', '3')
         _SALTS.set_setting('sort5_field', '4')
-        sso = ['Local', 'DirectDownload.tv', 'VKBox', 'NoobRoom', 'yify-streaming', 'stream-tv.co', 'streamallthis.is', 'PlayBox', 'GVCenter', 'clickplay.to', 'IceFilms', 'ororo.tv', 'afdah.org',
-                'xmovies8', 'Flixanity', 'hdmz', 'niter.tv', 'yify.tv', 'movietv.to', 'MintMovies', 'MovieNight', 'cmz', 'viooz.ac', 'view47', 'MoviesHD', 'OnlineMovies', 'MoviesOnline7',
+        sso = ['Local', 'DirectDownload.tv', 'VKBox', 'NoobRoom', 'yify-streaming', 'stream-tv.co', 'streamallthis.is', 'PlayBox', 'GVCenter', 'Shush.se', 'clickplay.to', 'IceFilms', 'ororo.tv',
+               'afdah.org', 'xmovies8', 'Flixanity', 'hdmz', 'niter.tv', 'yify.tv', 'movietv.to', 'MintMovies', 'MovieNight', 'cmz', 'viooz.ac', 'view47', 'MoviesHD', 'OnlineMovies', 'MoviesOnline7',
                 'wmo.ch', 'zumvo.com', 'alluc.com', 'MyVideoLinks.eu', 'OneClickWatch', 'tubemotion', 'RLSSource.net', 'TVRelease.Net', 'FilmStreaming.in', 'PrimeWire', 'WatchFree.to',
                'pftv', 'wso.ch', 'WatchSeries', 'SolarMovie', 'UFlix.org', 'ch131', 'moviestorm.eu', 'vidics.ch', 'Movie4K', 'LosMovies', 'MerDB', 'iWatchOnline', '2movies', 'iStreamHD', 'afdah',
                'filmikz.ch', 'movie25']
