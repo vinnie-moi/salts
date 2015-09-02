e657cef8189001d389f0561e94dfaad85c8a5112
Tracy Norris <tknorris@gmail.com>
2015-09-02 17:17:42 -0400

    Fix gvcenter

--- scrapers/gvcenter_scraper.py
+++ scrapers/gvcenter_scraper.py
@@ -32,19 +32,23 @@ from salts_lib import log_utils
 from salts_lib.constants import VIDEO_TYPES
 
 BASE_URL = 'http://www.gearscenter.com'
-SEARCH_URL = '/gold-server/gapiandroid205/?option=search&q=%s&page=1&total=0&block=0'
-CONTENT_URL = '/gold-server/gapiandroid205/?option=content&id=%s'
-SOURCE_URL = '/gold-server/gapiandroid205/?option=filmcontent&id=%s&cataid=0'
-EXTRA_URL = ('&os=android&version=2.0.5&versioncode=205&param_1=F2EF57A9374977FD431ECAED984BA7A2&'
-             'deviceid=%s&param_3=7326c76a03066b39e2a0b1dc235c351c&param_4=%s'
-             '&param_5=%s&token=%s&time=%s&devicename=Google-Nexus-%s-%s')
+SEARCH_URL = '/gold-server/gapiandroid206/?option=search&q=%s&page=1&total=0&block=0'
+CONTENT_URL = '/gold-server/gapiandroid206/?option=content&id=%s&sid=%s'
+SOURCE_URL = '/gold-server/gapiandroid206/?option=filmcontent&id=%s&cataid=%s&sid=%s'
+EXTRA_URL = ('&os=android&version=2.0.6&versioncode=206&param_1=EA2C2D2240456D78B2CCE8148B10A674'
+             '&deviceid=%s&param_3=0685257cd8bc8108d550c4e948aebf2f&param_4=%s'
+             '&param_5=%s&token=%s&time=%s&devicename=Google-Nexus-%s-%s'
+             '&sm=%s&si=%s&extra_1=%s&extra_2=%s&extra_3=%s')
 
 RESULT_URL = '/video_type=%s&catalog_id=%s'
 EPISODE_URL = RESULT_URL + '&season=%s&episode=%s'
 ANDROID_LEVELS = {'22': '5.1', '21': '5.0', '19': '4.4.4', '18': '4.3.0', '17': '4.2.0', '16': '4.1.0', '15': '4.0.4', '14': '4.0.2', '13': '3.2.0'}
 COUNTRIES = ['US', 'GB', 'CA', 'DK', 'MX', 'ES', 'JP', 'CN', 'DE', 'GR']
-DATA_KEY = base64.b64decode('M2FiYWFkMjE2NDYzYjc0MQ==')
-FILM_KEY = base64.b64decode('MmIyYTNkNTNkYzdiZjQyNw==')
+vc = urllib.quote_plus(str(206).encode('utf-8'))
+vn = urllib.quote_plus('2.0.6')
+pn = hashlib.md5('com.gamena.funboxhd').hexdigest().upper()
+FILM_KEY = hashlib.md5(vc + vn + pn).hexdigest()[0:16]
+URL_KEY = base64.b64decode('RzRtM2wwZnRfczNjcjN0MA==')
 GV_USER_AGENT = "Apache-HttpClient/UNAVAILABLE (java 1.4)"
 
 class GVCenter_Scraper(scraper.Scraper):
@@ -76,9 +80,12 @@ class GVCenter_Scraper(scraper.Scraper):
         sources = []
         if source_url:
             params = urlparse.parse_qs(source_url)
-            show_url = CONTENT_URL % (params['catalog_id'][0])
+            catalog_id = params['catalog_id'][0]
+            sid = hashlib.md5('content%scthd' % (catalog_id)).hexdigest()
+            show_url = CONTENT_URL % (catalog_id, sid)
             url = urlparse.urljoin(self.base_url, show_url)
             html = self._http_get(url, cache_limit=.5)
+            time.sleep(3)
             try:
                 js_data = json.loads(html)
                 if video.video_type == VIDEO_TYPES.EPISODE:
@@ -87,10 +94,12 @@ class GVCenter_Scraper(scraper.Scraper):
                 log_utils.log('Invalid JSON returned for: %s' % (url), xbmc.LOGWARNING)
             else:
                 for film in js_data['listvideos']:
-                    source_url = SOURCE_URL % (film['film_id'])
+                    film_id = film['film_id']
+                    sid = hashlib.md5('%s%scthd' % (film_id, catalog_id)).hexdigest()
+                    source_url = SOURCE_URL % (film_id, catalog_id, sid)
                     url = urlparse.urljoin(self.base_url, source_url)
-                    time.sleep(1.5)
                     html = self._http_get(url, cache_limit=.5)
+                    time.sleep(1.5)
                     try:
                         film_js = json.loads(html)
                     except ValueError:
@@ -141,7 +150,9 @@ class GVCenter_Scraper(scraper.Scraper):
 
     def _get_episode_url(self, show_url, video):
         params = urlparse.parse_qs(show_url)
-        source_url = CONTENT_URL % (params['catalog_id'][0])
+        catalog_id = params['catalog_id'][0]
+        sid = hashlib.md5('content%scthd' % (catalog_id)).hexdigest()
+        source_url = CONTENT_URL % (catalog_id, sid)
         url = urlparse.urljoin(self.base_url, source_url)
         html = self._http_get(url, cache_limit=.5)
         try:
@@ -165,30 +176,44 @@ class GVCenter_Scraper(scraper.Scraper):
                             return EPISODE_URL % (video.video_type, params['catalog_id'][0], int(season), int(episode))
 
     def _http_get(self, url, data=None, cache_limit=8):
-        url += self.__get_extra()
+        now = str(int(time.time()))
+        url += self.__get_extra(now)
         headers = {
                    'User-Agent': GV_USER_AGENT
         }
         result = super(GVCenter_Scraper, self)._cached_http_get(url, self.base_url, self.timeout, data=data, headers=headers, cache_limit=cache_limit)
         try:
+            #print 'result: %s' % (result)
             js_data = json.loads(result)
         except ValueError:
             log_utils.log('Invalid JSON returned for: %s' % (url), xbmc.LOGWARNING)
         else:
             if 'data' in js_data:
-                return self.__decrypt(DATA_KEY, base64.b64decode(js_data['data']))
-
+                key = hashlib.md5(now).hexdigest()[0:16]
+                return self.__decrypt(key, base64.b64decode(js_data['data']))
         return ''
                     
-    def __get_extra(self):
-        now = str(int(time.time()))
+    def __get_extra(self, now):
+        token = hashlib.md5(now).hexdigest()
         build = random.choice(ANDROID_LEVELS.keys())
-        device_id = hashlib.md5(str(random.randint(0, sys.maxint))).hexdigest()
         country = random.choice(COUNTRIES)
-        return EXTRA_URL % (device_id, country, country.lower(), hashlib.md5(now).hexdigest(), now, build, ANDROID_LEVELS[build])
+        device_id = '000000000000000'
+        sm = hashlib.md5(str(random.randint(0, 1000))).hexdigest()
+        si = hashlib.md5('catoon_206').hexdigest()
+        ex_1 = hashlib.md5(str(now) + sm).hexdigest()
+        ex_2 = urllib.quote_plus(hashlib.md5(str(now) + si).hexdigest())
+        ex_3 = sm[0:5] + hashlib.md5(device_id).hexdigest()[2:7]
+        ex_3 = urllib.quote_plus(base64.encodestring(self.__encrypt(URL_KEY, ex_3)))
+        return EXTRA_URL % (device_id, country, country.lower(), token, now, build, ANDROID_LEVELS[build], sm, si, ex_1, ex_2, ex_3)
     
     def __decrypt(self, key, cipher_text):
         decrypter = pyaes.Decrypter(pyaes.AESModeOfOperationECB(key))
         plain_text = decrypter.feed(cipher_text)
         plain_text += decrypter.feed()
         return plain_text
+    
+    def __encrypt(self, key, text):
+        encrypter = pyaes.Encrypter(pyaes.AESModeOfOperationECB(key))
+        ct = encrypter.feed(text)
+        ct += encrypter.feed()
+        return ct
