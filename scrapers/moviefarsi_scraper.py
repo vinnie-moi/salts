1f03977d42a54a2730e8e7232ff653fe1d38147c
Tracy Norris <tknorris@gmail.com>
2015-09-15 00:22:33 -0400

    add moviefarsi scraper

---
+++ scrapers/moviefarsi_scraper.py
@@ -0,0 +1,171 @@
+"""
+    SALTS XBMC Addon
+    Copyright (C) 2014 tknorris
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+import scraper
+import urllib
+import urlparse
+import re
+import xbmcaddon
+from salts_lib import dom_parser
+from salts_lib.constants import VIDEO_TYPES
+
+BASE_URL = 'http://moviefarsi2.com'
+TVSHOW_URLS = ['http://dl1.moviefarsi.org/serial/', 'http://dl1.moviefarsi.org/serial/best/', 'http://dl2.moviefarsi.org/serial/', 'http://dl3.moviefarsi.org/serial/', 'http://dl5.moviefarsi.org/serial/']
+
+class MovieFarsi_Scraper(scraper.Scraper):
+    base_url = BASE_URL
+
+    def __init__(self, timeout=scraper.DEFAULT_TIMEOUT):
+        self.timeout = timeout
+        self.base_url = xbmcaddon.Addon().getSetting('%s-base_url' % (self.get_name()))
+
+    @classmethod
+    def provides(cls):
+        return frozenset([VIDEO_TYPES.TVSHOW, VIDEO_TYPES.EPISODE, VIDEO_TYPES.MOVIE])
+
+    @classmethod
+    def get_name(cls):
+        return 'MovieFarsi'
+
+    def resolve_link(self, link):
+        return link
+
+    def format_source_label(self, item):
+        label = '[%s] %s' % (item['quality'], item['host'])
+        if 'views' in item and item['views']:
+            label += ' (%s views)' % item['views']
+        return label
+
+    def get_sources(self, video):
+        source_url = self.get_url(video)
+        hosters = []
+        if source_url:
+            if video.video_type == VIDEO_TYPES.EPISODE:
+                    sources = self.__get_files(source_url)
+                    for source in sources:
+                        _, season, episode, height, _ = self.__parse_episode_link(source['link'])
+                        if int(video.season) == int(season) and int(video.episode) == int(episode):
+                            hoster = {'multi-part': False, 'host': self._get_direct_hostname(source['url']), 'class': self, 'quality': self._height_get_quality(height), 'views': None, 'rating': None, 'url': source['url'], 'direct': True}
+                            hosters.append(hoster)
+            else:
+                source_url = urlparse.urljoin(self.base_url, source_url)
+                html = self._http_get(source_url, cache_limit=1)
+                for match in re.finditer('downloadicon.png.*?href="([^"]+)', html):
+                    stream_url = match.group(1)
+                    _title, _year, height, _extra = self.__parse_movie_link(stream_url)
+                    hoster = {'multi-part': False, 'host': self._get_direct_hostname(stream_url), 'class': self, 'quality': self._height_get_quality(height), 'views': None, 'rating': None, 'url': stream_url, 'direct': True}
+                    hosters.append(hoster)
+                    
+        return hosters
+
+    def __parse_episode_link(self, link):
+        match = re.match('(.*?)\.S(\d+)E(\d+)\.(\d+)p(?:_|\.)(.*)', link)
+        if match:
+            return match.groups()
+        else:
+            return ('', '-1', '-1', '-1', '')
+    
+    def __parse_movie_link(self, link):
+        file_name = link.split('/')[-1]
+        match = re.match('(.*?)(?:\.(\d{4}))?\.(\d+)p(?:\.|_)(.*)', file_name)
+        if match:
+            return match.groups()
+        else:
+            return ('', '', '480', '')  # make 480p when unknown
+    
+    def get_url(self, video):
+        return super(MovieFarsi_Scraper, self)._default_get_url(video)
+
+    def _get_episode_url(self, show_url, video):
+        force_title = self._force_title(video)
+        if not force_title:
+            html = self._http_get(show_url, cache_limit=24)
+            match = re.search('href="(S%02d/?)"' % (int(video.season)), html)
+            if match:
+                season_url = urlparse.urljoin(show_url, match.group(1))
+                for row in self.__parse_directory(self._http_get(season_url, cache_limit=24)):  # Quality directory
+                    url = urlparse.urljoin(season_url, row['link'])
+                    html = self._http_get(url, cache_limit=.5)
+                    match = re.search('href="[^"]*\.S%02dE%02d\.[^"]*' % (int(video.season), int(video.episode)), html)
+                    if match:
+                        return season_url
+            
+    def search(self, video_type, title, year):
+        results = []
+        norm_title = self._normalize_title(title)
+        if video_type == VIDEO_TYPES.TVSHOW:
+            for server_url in TVSHOW_URLS:
+                for row in self.__parse_directory(self._http_get(server_url, cache_limit=48)):
+                    match_year = ''
+                    if norm_title in self._normalize_title(row['title']) and (not year or not match_year or year == match_year):
+                        result = {'url': urlparse.urljoin(server_url, row['link']), 'title': row['title'], 'year': match_year}
+                        results.append(result)
+        else:
+            search_url = urlparse.urljoin(self.base_url, '/?s=')
+            search_url += urllib.quote_plus(title)
+            html = self._http_get(search_url, cache_limit=.5)
+            for article in dom_parser.parse_dom(html, 'article', {'class': 'entry-body'}):
+                link = dom_parser.parse_dom(article, 'a', {'class': 'more-link'}, 'href')
+                item = dom_parser.parse_dom(article, 'div', {'class': 'post-content'})
+                match = re.search('href="([^"]+).*?</a>\s*([^<]+)', item[0], re.DOTALL) if item else ''
+                if match and link:
+                    category, match_title_year = match.groups()
+                    if category.upper().endswith('/MOVIES'):
+                        match = re.search('(.*?)\s+\(?(\d{4})\)?', match_title_year)
+                        if match:
+                            match_title, match_year = match.groups()
+                        else:
+                            match_title = match_title_year
+                            match_year = ''
+                        
+                        if not year or not match_year or year == match_year:
+                            result = {'url': link[0].replace(self.base_url, ''), 'title': match_title, 'year': match_year}
+                            results.append(result)
+        
+        return results
+
+    def __get_files(self, url):
+        sources = []
+        for row in self.__parse_directory(self._http_get(url, cache_limit=.5)):
+            source_url = urlparse.urljoin(url, row['link'])
+            if row['directory']:
+                sources += self.__get_files(source_url)
+            else:
+                row['url'] = source_url
+                sources.append(row)
+        return sources
+    
+    def __parse_directory(self, html):
+        rows = []
+        for match in re.finditer('\s*<a\s+href="([^"]+)">([^<]+)</a>\s+(\d+-[a-zA-Z]+-\d+ \d+:\d+)\s+(-|\d+)', html):
+            link, title, date, size = match.groups()
+            if title.endswith('/'): title = title[:-1]
+            row = {'link': link, 'title': title, 'date': date}
+            if link.endswith('/'):
+                row['directory'] = True
+            else:
+                row['directory'] = False
+
+            if size == '-':
+                row['size'] = None
+            else:
+                row['size'] = size
+            rows.append(row)
+        return rows
+    
+    def _http_get(self, url, cache_limit=8):
+        return super(MovieFarsi_Scraper, self)._cached_http_get(url, self.base_url, self.timeout, cache_limit=cache_limit)
