5248599f435c410cd2137816856940e78bb86ef7
Tracy Norris <tknorris@gmail.com>
2015-09-15 00:32:40 -0400

    remove playbox scraper; add moviefarsi to auto-conf

--- default.py
+++ default.py
@@ -160,7 +160,7 @@ def auto_conf():
         kodi.set_setting('sort3_field', '1')
         kodi.set_setting('sort4_field', '3')
         kodi.set_setting('sort5_field', '4')
-        sso = ['Local', 'DirectDownload.tv', 'VKBox', 'NoobRoom', 'yify-streaming', 'stream-tv.co', 'streamallthis.is', 'Dizimag', 'Dizilab', 'PlayBox', 'GVCenter', 'MegaboxHD', 'Shush.se', 'clickplay.to',
+        sso = ['Local', 'DirectDownload.tv', 'VKBox', 'NoobRoom', 'yify-streaming', 'stream-tv.co', 'streamallthis.is', 'Dizimag', 'Dizilab', 'MovieFarsi', 'GVCenter', 'MegaboxHD', 'Shush.se', 'clickplay.to',
                'IceFilms', 'ororo.tv', 'afdah.org', 'xmovies8', 'Flixanity', 'hdmz', 'niter.tv', 'yify.tv', 'pubfilm', 'movietv.to', 'beinmovie', 'popcorntimefree', 'tunemovie', 'MintMovies', 'MovieNight',
                'cmz', 'viooz.ac', 'view47', 'MoviesHD', 'OnlineMovies', 'MoviesOnline7', 'wmo.ch', 'zumvo.com', 'mvsnap', 'alluc.com', 'MyVideoLinks.eu', 'OneClickWatch', 'RLSSource.net',
                'TVRelease.Net', 'FilmStreaming.in', 'PrimeWire', 'WatchFree.to', 'CouchTunerV2', 'CouchTunerV1', 'Watch8Now', 'pftv', 'wso.ch', 'WatchSeries', 'SolarMovie', 'UFlix.org', 'ch131',
