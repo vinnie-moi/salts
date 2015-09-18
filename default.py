abe01296d41ed3970bfd48fc38b310368eb0cbb6
Tracy Norris <tknorris@gmail.com>
2015-09-18 02:52:10 -0400

    Bump to 1.0.49

--- default.py
+++ default.py
@@ -160,9 +160,9 @@ def auto_conf():
         kodi.set_setting('sort3_field', '1')
         kodi.set_setting('sort4_field', '3')
         kodi.set_setting('sort5_field', '4')
-        sso = ['Local', 'DirectDownload.tv', 'VKBox', 'NoobRoom', 'OneClickTVShows', 'yify-streaming', 'stream-tv.co', 'streamallthis.is', 'Dizimag', 'Dizilab', 'MovieFarsi', 'GVCenter',
-               'MegaboxHD', 'Shush.se', 'Dizigold', 'clickplay.to', 'IceFilms', 'ororo.tv', 'afdah.org', 'xmovies8', 'OnlineMoviesIs', 'OnlineMoviesPro', 'Flixanity', 'hdmz', 'niter.tv', 'yify.tv',
-               'pubfilm', 'movietv.to', 'beinmovie', 'popcorntimefree', 'tunemovie', 'MintMovies', 'MovieNight', 'cmz', 'viooz.ac', 'view47', 'MoviesHD', 'OnlineMovies', 'MoviesOnline7',
+        sso = ['Local', 'DirectDownload.tv', 'VKBox', 'NoobRoom', 'OneClickTVShows', '123Movies', 'yify-streaming', 'stream-tv.co', 'streamallthis.is', 'Dizimag', 'Dizilab', 'MovieFarsi',
+               'GVCenter', 'MegaboxHD', 'Shush.se', 'Dizigold', 'clickplay.to', 'IceFilms', 'ororo.tv', 'afdah.org', 'xmovies8', 'OnlineMoviesIs', 'OnlineMoviesPro', 'Flixanity', 'hdmz', 'niter.tv',
+               'yify.tv', 'pubfilm', 'movietv.to', 'beinmovie', 'popcorntimefree', 'tunemovie', 'MintMovies', 'MovieNight', 'cmz', 'viooz.ac', 'view47', 'MoviesHD', 'OnlineMovies', 'MoviesOnline7',
                'wmo.ch', 'zumvo.com', 'mvsnap', 'alluc.com', 'MyVideoLinks.eu', 'OneClickWatch', 'RLSSource.net', 'TVRelease.Net', 'FilmStreaming.in', 'PrimeWire', 'WatchFree.to',
                'CouchTunerV2', 'CouchTunerV1', 'Watch8Now', 'pftv', 'wso.ch', 'WatchSeries', 'SolarMovie', 'UFlix.org', 'ch131', 'ayyex', 'moviestorm.eu', 'vidics.ch', 'Movie4K', 'LosMovies',
                'MerDB', 'iWatchOnline', '2movies', 'iStreamHD', 'afdah', 'filmikz.ch', 'movie25']
