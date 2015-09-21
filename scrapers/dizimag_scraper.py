c488c1d27cbbc7c3b09c30be3d46addf8494551a
Tracy Norris <tknorris@gmail.com>
2015-09-21 12:38:46 -0400

    fix dizimag

--- scrapers/dizimag_scraper.py
+++ scrapers/dizimag_scraper.py
@@ -18,9 +18,6 @@
 import scraper
 import re
 import urlparse
-import json
-import xbmc
-import time
 from salts_lib import log_utils
 from salts_lib import dom_parser
 from salts_lib.constants import VIDEO_TYPES
@@ -28,10 +25,6 @@ from salts_lib.constants import USER_AGENT
 from salts_lib import kodi
 
 BASE_URL = 'http://dizimag.co'
-GIVEVIDEO_URL = '/service/givevideo'
-DMG_URL1 = '/service/idmg?type=%s&a=%s&b=1%s&s=%s&e=%s&_=%s'
-DMG_URL2 = '/service/vdmg?type=%s&a=%s&b=1%s&s=%s&e=%s&_=%s'
-XHR = {'X-Requested-With': 'XMLHttpRequest'}
 
 class Dizimag_Scraper(scraper.Scraper):
     base_url = BASE_URL
@@ -62,46 +55,24 @@ class Dizimag_Scraper(scraper.Scraper):
             page_url = urlparse.urljoin(self.base_url, source_url)
             html = self._http_get(page_url, cache_limit=.5)
             
-            for match in re.finditer('onclick\s*=\s*"Change_Source\(\s*(\d+)\s*,\s*\'([^\']+)\'\s*\)', html):
-                vid_id, host = match.groups()
-                if host == 'Roosy':
-                    url_type = host
-                    a = 'awQa5s14d5s6s12s'
-                    dmg_url = DMG_URL2
-                else:
-                    url = urlparse.urljoin(self.base_url, GIVEVIDEO_URL)
-                    data = {'i': vid_id, 'n': host, 'p': 0}
-                    html = self._http_get(url, data=data, headers=XHR, cache_limit=0)
+            for alter_link in dom_parser.parse_dom(html, 'a', {'class': 'alterlink'}, 'href'):
+                if not alter_link.endswith('=0'):
+                    alter_url = page_url + alter_link
+                    html = self._http_get(alter_url, cache_limit=.5)
                 
-                    try:
-                        js_data = json.loads(html)
-                    except ValueError:
-                        log_utils.log('Invalid JSON returned: %s: %s' % (url, html), xbmc.LOGWARNING)
-                    else:
-                        url_type = js_data['p']['tur']
-                        a = js_data['p']['c']
-                        dmg_url = DMG_URL1
-
-                b = vid_id
-                now = int(time.time() * 1000)
-                match = re.search('/([^/]+)/(.*)-izle-dizi', source_url)
-                if match:
-                    s, e = match.groups()
-                    dmg_url = dmg_url % (url_type, a, b, s, e, now)
-                    url = urlparse.urljoin(self.base_url, dmg_url)
-                    html = self._http_get(url, headers=XHR, cache_limit=1)
-                    match = re.search('var\s+kaynaklar\s*=\s*\[([^]]+)', html)
+                for script in re.finditer('<script[^>]*>(.*?)</script>', html, re.DOTALL):
+                    match = re.search('var\s+kaynaklar\s*=\s*\[([^]]+)', script.group(1))
                     if match:
                         for match in re.finditer('file\s*:\s*"([^"]+)"\s*,\s*label\s*:\s*"(\d+)p?"', match.group(1)):
                             stream_url, height = match.groups()
                             stream_url = stream_url.decode('unicode_escape')
                             host = self._get_direct_hostname(stream_url)
-                            if  host == 'gvideo':
+                            if host == 'gvideo':
                                 quality = self._gv_get_quality(stream_url)
                             else:
                                 quality = self._height_get_quality(height)
                                 stream_url += '|User-Agent=%s&Referer=%s' % (USER_AGENT, page_url)
-
+    
                             hoster = {'multi-part': False, 'host': host, 'class': self, 'quality': quality, 'views': None, 'rating': None, 'url': stream_url, 'direct': True}
                             hosters.append(hoster)
     
