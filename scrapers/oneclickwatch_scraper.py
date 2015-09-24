55741a098787f88a034bba01fbfe2c717c3805d5
Tracy Norris <tknorris@gmail.com>
2015-09-24 00:49:50 -0400

    fix oneclickwatch

--- scrapers/oneclickwatch_scraper.py
+++ scrapers/oneclickwatch_scraper.py
@@ -83,12 +83,14 @@ class OneClickWatch_Scraper(scraper.Scraper):
 
     def search(self, video_type, title, year):
         html = self._http_get(self.base_url, cache_limit=.25)
-        match = re.search('id="ocw_once_search[^>]*value="([^"]+)', html)
-        if match:
-            token = match.group(1)
-            search_url = urlparse.urljoin(self.base_url, '/?s=%s&ocw_once_search=%s' % (urllib.quote_plus(title), token))
-            headers = {'Referer': self.base_url}
-            html = self._http_get(search_url, headers=headers, cache_limit=.25)
+        extra = ''
+        for match in re.finditer('<input\s+type="hidden"[^>]+name="([^"]+)"\s+value="([^"]+)', html):
+            extra += '&%s=%s' % (match.group(1), urllib.quote(match.group(2)))
+            
+        search_url = urlparse.urljoin(self.base_url, '/?s=%s' % (urllib.quote_plus(title)))
+        search_url += extra
+        headers = {'Referer': self.base_url}
+        html = self._http_get(search_url, headers=headers, cache_limit=.25)
 
         pattern = 'class="title"><a href="(?P<url>[^"]+)[^>]+>(?P<post_title>[^<]+).*?rel="bookmark">(?P<date>[^<]+)'
         date_format = '%B %d, %Y'
