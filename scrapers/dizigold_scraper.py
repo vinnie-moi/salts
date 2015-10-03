7aa49f9f80e2f94c7e3742ab225e9330b549c44c
Tracy Norris <tknorris@gmail.com>
2015-10-03 01:36:20 -0400

    fix dizigold scraper

--- scrapers/dizigold_scraper.py
+++ scrapers/dizigold_scraper.py
@@ -86,29 +86,9 @@ class Dizigold_Scraper(scraper.Scraper):
         return super(Dizigold_Scraper, self)._default_get_url(video)
 
     def _get_episode_url(self, show_url, video):
-        show_url = urlparse.urljoin(self.base_url, show_url)
-        html = self._http_get(show_url, cache_limit=8)
-        dizi_id = dom_parser.parse_dom(html, 'div', {'id': 'icerikid'}, 'value')
-        if dizi_id:
-            season_data = {'sezon_id': video.season, 'dizi_id': dizi_id[0], 'tip': 'sezon'}
-            html = self._http_get(self.ajax_url, data=season_data, headers=XHR, cache_limit=0)
-            html = re.sub(r'\\n|\\t|\\', '', html)
-            force_title = self._force_title(video)
-
-            if not force_title:
-                episode_pattern = 'href="([^"]+/%s-sezon/%s-bolum/?)' % (video.season, video.episode)
-                match = re.search(episode_pattern, html, re.DOTALL)
-                if match:
-                    return match.group(1).replace(self.base_url, '')
-            
-            if (force_title or kodi.get_setting('title-fallback') == 'true') and video.ep_title:
-                norm_title = self._normalize_title(video.ep_title)
-                for item in dom_parser.parse_dom(html, 'div', {'class': '[^"]*playlist-content[^"]*'}):
-                    match = re.search('href="([^"]+)', item)
-                    ep_title = dom_parser.parse_dom(item, 'p', {'class': 'realcuf'})
-                    if match and ep_title:
-                        if norm_title == self._normalize_title(ep_title[0]):
-                            return match.group(1).replace(self.base_url, '')
+        episode_pattern = 'href="([^"]+/%s-sezon/%s-[^"]*bolum[^"]*)' % (video.season, video.episode)
+        title_pattern = 'href="([^"]+)"\s+class="realcuf".*?<p\s+class="realcuf">([^<]+)'
+        return super(Dizigold_Scraper, self)._default_get_episode_url(show_url, video, episode_pattern, title_pattern)
 
     def search(self, video_type, title, year):
         html = self._http_get(self.base_url, cache_limit=8)
