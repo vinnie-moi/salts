66e54339158ca4d02d561a77371fc99f863160e8
Tracy Norris <tknorris@gmail.com>
2015-10-05 13:07:36 -0400

    fix xmovies8

--- scrapers/xmovies8_scraper.py
+++ scrapers/xmovies8_scraper.py
@@ -19,15 +19,18 @@ import scraper
 import urllib
 import urlparse
 import re
-import random
 from salts_lib import kodi
 from salts_lib import dom_parser
 from salts_lib.constants import VIDEO_TYPES
 from salts_lib.constants import USER_AGENT
 from salts_lib.constants import XHR
-from salts_lib.constants import QUALITIES
+
+BASE_URL = 'https://xmovies8.org'
+VIDEO_URL = '/video_info/html5'
 
 class XMovies8_Scraper(scraper.Scraper):
+    base_url = BASE_URL
+
     def __init__(self, timeout=scraper.DEFAULT_TIMEOUT):
         self.timeout = timeout
         self.base_url = kodi.get_setting('%s-base_url' % (self.get_name()))
@@ -52,70 +55,45 @@ class XMovies8_Scraper(scraper.Scraper):
         if source_url:
             url = urlparse.urljoin(self.base_url, source_url)
             html = self._http_get(url, cache_limit=.5)
-            for match in re.finditer('href="([^"]+)[^>]*>(\d+)x(\d+)', html):
-                stream_url, width, _ = match.groups()
-                hoster = {'multi-part': False, 'host': self._get_direct_hostname(stream_url), 'class': self, 'quality': self._width_get_quality(width), 'views': None, 'rating': None, 'url': stream_url, 'direct': True}
-                hosters.append(hoster)
-            
-            match = re.search('url\s*:\s*"([^"]+picasa\.php).*?data\s*:\s*\'([^\']+)', html, re.DOTALL)
+            match = re.search('video_id\s*=\s*"([^"]+)', html)
             if match:
-                php_url, data = match.groups()
-                html = self._http_get(php_url, data=data, headers=XHR, cache_limit=0)
-                match = re.search('<source\s+src="([^"]+)', html)
-                if match:
-                    stream_url = match.group(1) + '|User-Agent=%s' % (USER_AGENT)
-                    hoster = {'multi-part': False, 'host': self._get_direct_hostname(stream_url), 'class': self, 'quality': QUALITIES.HD720, 'views': None, 'rating': None, 'url': stream_url, 'direct': True}
+                data = {'v': match.group(1)}
+                url = urlparse.urljoin(self.base_url, VIDEO_URL)
+                html = self._http_get(url, data=data, headers=XHR, cache_limit=0)
+                for match in re.finditer('<source\s+data-res="([^"]+)"\s+src="([^"]+)', html):
+                    stream_url = urlparse.urljoin(self.base_url, match.group(2)) + '|User-Agent=%s' % (USER_AGENT)
+                    quality = self._height_get_quality(match.group(1))
+                    hoster = {'multi-part': False, 'host': self._get_direct_hostname(stream_url), 'class': self, 'quality': quality, 'views': None, 'rating': None, 'url': stream_url, 'direct': True}
                     hosters.append(hoster)
-            
-            match = re.search('<source\s+src="([^"]+)', html)
-            if match:
-                stream_url = match.group(1) + '|User-Agent=%s' % (USER_AGENT)
-                hoster = {'multi-part': False, 'host': self._get_direct_hostname(stream_url), 'class': self, 'quality': QUALITIES.HD720, 'views': None, 'rating': None, 'url': stream_url, 'direct': True}
-                hosters.append(hoster)
             
         return hosters
 
     def get_url(self, video):
         return super(XMovies8_Scraper, self)._default_get_url(video)
 
-    @classmethod
-    def get_settings(cls):
-        settings = super(XMovies8_Scraper, cls).get_settings()
-        settings.append('         <setting id="%s-default_url" type="string" visible="false"/>' % (cls.get_name()))
-        return settings
-
     def search(self, video_type, title, year):
-        search_url = urlparse.urljoin(self.base_url, '/?s=%s' % urllib.quote_plus(title))
+        search_url = urlparse.urljoin(self.base_url, '/results?q=%s' % urllib.quote_plus(title))
         html = self._http_get(search_url, allow_redirect=False, cache_limit=.25)
         results = []
-        if html.startswith('http://') and '/movie/' in html:
-            result = {'url': html.replace(self.base_url, ''), 'title': title, 'year': year}
-            results.append(result)
-        else:
-            for result in dom_parser.parse_dom(html, 'div', {'class': 'info'}):
-                match = re.search('href="([^"]+)"[^>]*>\s*([^<]+)', result)
+        for result in dom_parser.parse_dom(html, 'div', {'class': 'cell'}):
+            match = re.search('class="video_title".*?href="([^"]+)"[^>]*>\s*([^<]+)', result, re.DOTALL)
+            if match:
+                url, match_title_year = match.groups()
+                match = re.search('(.*?)\s+\((\d{4})\)', match_title_year)
                 if match:
-                    url, match_title_year = match.groups()
-                    match = re.search('(.*?)\s+\((\d{4})\)', match_title_year)
+                    match_title, match_year = match.groups()
+                else:
+                    match_title = match_title_year
+                    match = re.search('class="video_quality".*?Year\s*(?:</b>)?\s*:\s*(\d{4})', result, re.DOTALL)
                     if match:
-                        match_title, match_year = match.groups()
+                        match_year = match.group(1)
                     else:
-                        match_title = match_title_year
                         match_year = ''
-    
-                    if not year or not match_year or year == match_year:
-                        result = {'url': url.replace(self.base_url, ''), 'title': match_title, 'year': match_year}
-                        results.append(result)
+
+                if not year or not match_year or year == match_year:
+                    result = {'url': url.replace(self.base_url, ''), 'title': match_title, 'year': match_year}
+                    results.append(result)
         return results
 
     def _http_get(self, url, cookies=None, data=None, headers=None, allow_redirect=True, cache_limit=8):
         return super(XMovies8_Scraper, self)._cached_http_get(url, self.base_url, self.timeout, cookies=cookies, data=data, headers=headers, allow_redirect=allow_redirect, cache_limit=cache_limit)
-
-# if no default url has been set, then pick one and set it. If one has been set, use it
-default_url = kodi.get_setting('%s-default_url' % (XMovies8_Scraper.get_name()))
-if not default_url:
-    BASE_URL = random.choice(['http://xmovies8.tv', 'http://megashare9.tv'])
-    XMovies8_Scraper.base_url = BASE_URL
-    kodi.set_setting('%s-default_url' % (XMovies8_Scraper.get_name()), BASE_URL)
-else:
-    XMovies8_Scraper.base_url = default_url
