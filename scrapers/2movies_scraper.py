23b85f69b7beb4aeb335985dc35efea65785c89b
Tracy Norris <tknorris@gmail.com>
2015-10-13 19:36:47 -0400

    update 2movies ajax url

--- scrapers/2movies_scraper.py
+++ scrapers/2movies_scraper.py
@@ -26,7 +26,7 @@ from salts_lib.constants import QUALITIES
 from salts_lib.constants import XHR
 
 BASE_URL = 'http://twomovies.us'
-AJAX_URL = '/Xajax/saveaction/'
+AJAX_URL = '/Xajax/ajaxifyy/'
 
 class TwoMovies_Scraper(scraper.Scraper):
     base_url = BASE_URL
