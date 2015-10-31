b9acc4c240891543c6e7445a6ffacf757bd494cc
Tracy Norris <tknorris@gmail.com>
2015-10-30 22:50:53 -0400

    fix torba.se audio

--- scrapers/torbase_scraper.py
+++ scrapers/torbase_scraper.py
@@ -20,6 +20,9 @@ import urllib
 import urlparse
 import re
 import json
+import xbmcvfs
+import xbmc
+import os
 from salts_lib import log_utils
 from salts_lib import kodi
 from salts_lib.constants import VIDEO_TYPES
@@ -30,6 +33,13 @@ from salts_lib.constants import QUALITIES
 BASE_URL = 'http://torba.se'
 SEARCH_URL = '/api/movies/list.json?genres=All+genres&limit=40&order=recent&q=%s&year=All+years'
 PLAYER_URL = '/api/movies/player.json?slug=%s'
+M3U8_PATH = os.path.join(xbmc.translatePath(kodi.get_profile()), 'torbase.m3u8')
+M3U8_TEMPLATE = [
+    '#EXTM3U',
+    '#EXT-X-MEDIA:TYPE=AUDIO,GROUP-ID="{audio_group}",DEFAULT=YES,AUTOSELECT=YES,NAME="Stream 1",URI="{audio_stream}"',
+    '',
+    '#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH={bandwidth},NAME="{stream_name}",AUDIO="{audio_group}"',
+    '{video_stream}']
 
 class TorbaSe_Scraper(scraper.Scraper):
     base_url = BASE_URL
@@ -47,7 +57,18 @@ class TorbaSe_Scraper(scraper.Scraper):
         return 'torba.se'
 
     def resolve_link(self, link):
-        return link
+        try:
+            xbmcvfs.delete(M3U8_PATH)
+            query = urlparse.parse_qs(link)
+            query = dict([(key, query[key][0]) if query[key] else (key, '') for key in query])
+            f = xbmcvfs.File(M3U8_PATH, 'w')
+            for line in M3U8_TEMPLATE:
+                line = line.format(**query)
+                f.write(line + '\n')
+            f.close()
+            return M3U8_PATH
+        except:
+            return None
 
     def format_source_label(self, item):
         label = '[%s] %s' % (item['quality'], item['host'])
@@ -70,24 +91,35 @@ class TorbaSe_Scraper(scraper.Scraper):
                 if match:
                     pl_url = urlparse.urljoin(st_url, match.group(1))
                     playlist = self._http_get(pl_url, cache_limit=.5)
-                    sources = self.__get_streams_from_m3u8(playlist.split('\n'))
+                    sources = self.__get_streams_from_m3u8(playlist.split('\n'), st_url)
                     for source in sources:
-                        stream_url = urlparse.urljoin(st_url, source)
-                        hoster = {'multi-part': False, 'host': self._get_direct_hostname(stream_url), 'class': self, 'quality': sources[source], 'views': None, 'rating': None, 'url': stream_url, 'direct': True}
+                        hoster = {'multi-part': False, 'host': self._get_direct_hostname(source), 'class': self, 'quality': sources[source], 'views': None, 'rating': None, 'url': source, 'direct': True}
                         hosters.append(hoster)
                 
         return hosters
 
-    def __get_streams_from_m3u8(self, playlist):
+    def __get_streams_from_m3u8(self, playlist, st_url):
         sources = {}
         quality = QUALITIES.HIGH
+        audio_group = ''
+        audio_stream = ''
+        stream_name = 'Unknown'
+        bandwidth = 0
         for line in playlist:
-            if line.startswith('#EXT-X-STREAM-INF'):
-                match = re.search('NAME="(\d+)p', line)
+            if line.startswith('#EXT-X-MEDIA'):
+                match = re.search('GROUP-ID="([^"]+).*?URI="([^"]+)', line)
                 if match:
-                    quality = self._height_get_quality(match.group(1))
+                    audio_group, audio_stream = match.groups()
+            if line.startswith('#EXT-X-STREAM-INF'):
+                match = re.search('BANDWIDTH=(\d+).*?NAME="(\d+p)', line)
+                if match:
+                    bandwidth, stream_name = match.groups()
+                    quality = self._height_get_quality(stream_name)
             elif line.endswith('m3u8'):
-                sources[line] = quality
+                stream_url = urlparse.urljoin(st_url, line)
+                query = {'audio_group': audio_group, 'audio_stream': audio_stream, 'stream_name': stream_name, 'bandwidth': bandwidth, 'video_stream': stream_url}
+                stream_url = urllib.urlencode(query)
+                sources[stream_url] = quality
                 
         return sources
         
