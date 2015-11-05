198da8d0f6d5fb26ca3bc2e2dbe3a453990934f3
Tracy Norris <tknorris@gmail.com>
2015-11-05 16:40:03 -0500

    fix dizibox

--- scrapers/dizibox_scraper.py
+++ scrapers/dizibox_scraper.py
@@ -20,14 +20,20 @@ import scraper
 import re
 import urlparse
 import urllib
+import json
 from salts_lib import dom_parser
+from salts_lib import log_utils
 from salts_lib.constants import VIDEO_TYPES
 from salts_lib.constants import FORCE_NO_MATCH
 from salts_lib.constants import USER_AGENT
+from salts_lib.constants import QUALITIES
+from salts_lib.constants import XHR
 from salts_lib import kodi
 
 BASE_URL = 'http://www.dizibox.com'
-ALTERNATIVES = ['1080P', 'İngilizce', 'Altyazısız']
+OK_META_URL = 'http://ok.ru/dk?cmd=videoPlayerMetadata&mid=%s'
+ALTERNATIVES = ['DBX', '1080P', '1080p', 'İngilizce', 'Altyazısız']
+OK_QUALITIES = {'MOBILE': QUALITIES.LOW, 'LOWEST': QUALITIES.MEDIUM, 'LOW': QUALITIES.MEDIUM, 'SD': QUALITIES.HIGH, 'HD': QUALITIES.HD720}
 
 class Dizibox_Scraper(scraper.Scraper):
     base_url = BASE_URL
@@ -60,11 +66,17 @@ class Dizibox_Scraper(scraper.Scraper):
             page_url = urlparse.urljoin(self.base_url, source_url)
             html = self._http_get(page_url, cache_limit=.5)
             hosters += self.__get_links(html, page_url)
+            if 'altyazi.png' in html:
+                for hoster in hosters:
+                    hoster['subs'] = True
 
             for match in re.finditer("<option\s+value='([^']+)[^>]+>([^<]+)", html):
                 if match.group(2) in ALTERNATIVES:
                     html = self._http_get(match.group(1), cache_limit=.5)
                     hosters += self.__get_links(html, page_url)
+                    if match.group(2) != 'Altyazısız':
+                        for hoster in hosters:
+                            hoster['subs'] = True
         
         return hosters
 
@@ -74,20 +86,45 @@ class Dizibox_Scraper(scraper.Scraper):
         fragment = dom_parser.parse_dom(html, 'span', {'class': 'object-wrapper'})
         iframe_src = dom_parser.parse_dom(fragment[0], 'iframe', ret='src')
         iframe_html = self._http_get(iframe_src[0], cache_limit=.10)
-        for match in re.finditer('"?file"?\s*:\s*"([^"]+)"\s*,\s*"?label"?\s*:\s*"(\d+)p?"', iframe_html):
-            stream_url, height = match.groups()
-            stream_url = stream_url.replace('\\&', '&')
-            if stream_url in seen_urls: continue
-            seen_urls[stream_url] = True
-            host = self._get_direct_hostname(stream_url)
-            if host == 'gvideo':
-                quality = self._gv_get_quality(stream_url)
-            else:
-                quality = self._height_get_quality(height)
-                stream_url += '|User-Agent=%s&Referer=%s' % (USER_AGENT, urllib.quote(page_url))
-            hoster = {'multi-part': False, 'host': host, 'class': self, 'quality': quality, 'views': None, 'rating': None, 'url': stream_url, 'direct': True}
-            if 'altyazi.png' in html: hoster['subs'] = True
-            sources.append(hoster)
+        match = re.search('src="([^"]*ok.ru/videoembed/[^"]+)', iframe_html)
+        if match:
+            sources = self.__get_ok_links(match.group(1))
+        else:
+            for match in re.finditer('"?file"?\s*:\s*"([^"]+)"\s*,\s*"?label"?\s*:\s*"(\d+)p?"', iframe_html):
+                stream_url, height = match.groups()
+                stream_url = stream_url.replace('\\&', '&')
+                if stream_url in seen_urls: continue
+                seen_urls[stream_url] = True
+                host = self._get_direct_hostname(stream_url)
+                if host == 'gvideo':
+                    quality = self._gv_get_quality(stream_url)
+                else:
+                    quality = self._height_get_quality(height)
+                    stream_url += '|User-Agent=%s&Referer=%s' % (USER_AGENT, urllib.quote(page_url))
+                hoster = {'multi-part': False, 'host': host, 'class': self, 'quality': quality, 'views': None, 'rating': None, 'url': stream_url, 'direct': True}
+                sources.append(hoster)
+        return sources
+    
+    def __get_ok_links(self, embed_url):
+        sources = []
+        match = re.search('videoembed/(.*)', embed_url)
+        if match:
+            meta_url = OK_META_URL % (match.group(1))
+            headers = XHR
+            headers['Referer'] = embed_url
+            html = self._http_get(meta_url, headers=headers, cache_limit=.5)
+            if html:
+                try:
+                    js_result = json.loads(html)
+                except ValueError:
+                    log_utils.log('Invalid JSON returned: %s: %s' % (meta_url, html), log_utils.LOGWARNING)
+                else:
+                    if 'videos' in js_result:
+                        for video in js_result['videos']:
+                            stream_url = video['url'] + '|User-Agent=%s&Referer=%s' % (USER_AGENT, urllib.quote(embed_url))
+                            quality = OK_QUALITIES.get(video['name'].upper(), QUALITIES.HIGH)
+                            hoster = {'multi-part': False, 'host': self._get_direct_hostname(video['url']), 'class': self, 'quality': quality, 'views': None, 'rating': None, 'url': stream_url, 'direct': True}
+                            sources.append(hoster)
         return sources
     
     def get_url(self, video):
